331d0ae22f3f14d64be29b252fe35c96dce71256
Rémy Greinhofer <remy.greinhofer@gmail.com>
2019-10-19 20:13:01 -0500

    Add working pgclient

--- pgclient.py
+++ pgclient.py
@@ -1,19 +1,35 @@
 import asyncio
+import os
 
 import aiopg
+import rapidtables
 
-dsn = "dbname=aiopg user=postgres password=postgres host=192.168.99.105 port=31718"
+hostname = os.environ["NODE_IP"]
+port = os.environ["NODE_PORT"]
+dsn = f"dbname=postgres user=postgres password=postgres host={hostname} port={port}"
+
+factbook_select_all_query = """
+SELECT year, date, shares, trades, dollars
+FROM factbook
+"""
+
+
+query = factbook_select_all_query
 
 
 async def go():
     pool = await aiopg.create_pool(dsn)
     async with pool.acquire() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("SELECT 1")
-            ret = []
+            await cur.execute(query)
+            headers = [d.name for d in cur.description]
+            data = []
             async for row in cur:
-                ret.append(row)
-            assert ret == [(1,)]
+                data.append(dict(zip(headers, row)))
+    pool.close()
+    await pool.wait_closed()
+    print("ALL DONE")
+    rapidtables.print_table(data)
 
 
 if __name__ == "__main__":
--- setup/pgsql-connect.sh
+++ setup/pgsql-connect.sh
@@ -2,5 +2,6 @@
 
 export POSTGRES_PASSWORD=$(kubectl get secret --namespace default postgresql -o jsonpath="{.data.postgresql-password}" | base64 --decode)
 # export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
+export NODE_IP=$(minikube ip)
 export NODE_PORT=$(kubectl get --namespace default -o jsonpath="{.spec.ports[0].nodePort}" services postgresql)
-echo "pgcli postgres://postgres:${POSTGRES_PASSWORD}@$(minikube ip):${NODE_PORT}/postgres"
+echo "pgcli postgres://postgres:${POSTGRES_PASSWORD}@${NODE_IP}:${NODE_PORT}/postgres"
